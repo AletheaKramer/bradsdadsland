9d01e81b6a321f12bd6b4214b3fef1f4b757b68b
Nicholas <nicholas@vanzant.ca>
2024-12-23 22:51:41 -0800

    Update Policies.jsx

--- src/components/Policies.jsx
+++ src/components/Policies.jsx
@@ -45,7 +45,7 @@ const Policies = () => {
         </ul>
 
         <h3 className="text-2xl font-semibold mt-6">
-          Glamping, Vintage T and RV Rentals:
+          Glamping and RV Rentals:
         </h3>
         <ul className="list-disc list-inside text-lg">
           <li>
